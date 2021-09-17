3d88b2dcddb0b24936b9d20054be94e41553906d
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-17 16:02:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ document.addEventListener('DOMContentLoaded',() =>{
 
 
     const width = 10
-    let currentIndex = 0
+    let currentIndex = 0 //for the first div
+    let appleIndex = 0 //first div
+    let currentSnake = [2,1,0] // 2 for the head, 0-1 being the body and tail
+
+
+function control(e){
+    //different keys for the user to use to control the snake
+
+
+}
+
 
 })
