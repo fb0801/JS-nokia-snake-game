fefb00dd03a76a5703517b1ce0bd46bb32f48c59
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-17 15:50:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',() =>{
+
+    const squares = document.querySelectorAll('.grid div') //gather all the .grid div elements that will be our squares
+    const scoreDisplay = document.querySelector('span') // get our span element where we will put the score
+    const startBtn = document.querySelector('.start') // get our start btn
+
+
+
+    const width = 10
+    let currentIndex = 0
+
+})
